0e3feb37c95a5ed6c6ff7d3aafa36fe79018a0ed
Petra <81535499+Petra-eng-hub@users.noreply.github.com>
2022-03-02 11:14:24 +0100

    Day1 done

---
+++ Petra/Day1_helloworld.dart
@@ -0,0 +1,12 @@
+main() {
+  // Printing the text 'Hello World'
+  print(
+    "Hello World, Day1_dart",
+  );
+  print(
+    "I wish we try the input/output thingy",
+  );
+}
+//Challenge for Day1, where we print an output
+//The main function has been declared so i will just go ahead
+
