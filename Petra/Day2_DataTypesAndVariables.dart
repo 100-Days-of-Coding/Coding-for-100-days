56bd34703135438934ca703ab909b946d213361e
Petra <81535499+Petra-eng-hub@users.noreply.github.com>
2022-03-05 05:02:33 +0100

    day 2 challenge and stuff

---
+++ Petra/Day2_DataTypesAndVariables.dart
@@ -0,0 +1,34 @@
+main() {
+  int MyAge = 24; //Well, not yet but soon
+  print(MyAge);
+
+  num FirstNumber = 2;
+  num SecondNumber = 4;
+  num ThirdNumber = FirstNumber + SecondNumber;
+//See how i added to have 24? Lmao
+  print(FirstNumber);
+  print(SecondNumber);
+  print(ThirdNumber);
+//so strings have various syntax, cool
+//single quotes, double quotes, with and without escape character\
+  print('Pdoc now');
+  print("Pdoc again");
+  print('P\doc now');
+  print("Pdoc again");
+  //the escape character \ is to indicate that ' doesn't close the string
+  //now defining strings
+  String name = "PDoc";
+  print(name);
+  //+ is used to concaternate strings
+  String surname = "Petra";
+  String givenname = " Ukeh";
+  String nameo = (surname + givenname);
+  print(nameo);
+  //To have space, i put it in Ukeh else it will just join the wholething
+  //interpolation,use $
+  //for multiple lines, you can use enter and when the lines finally finish, the ; comes in
+  //for the challenge
+  var agen = 23;
+  var namen = "Petra";
+  print('$namen is $agen years old');
+}
