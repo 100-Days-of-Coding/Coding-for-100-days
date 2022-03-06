35c36e4eb44cd986306905c1d171b5e6b1cd3686
andrew21-mch <nfonandrew73@gmail.com>
2022-03-06 12:31:04 +0100

    task 1

--- Andy/tasks/day_1.dart
+++ Andy/tasks/day_1.dart
@@ -1,4 +1,5 @@
 main() {
   String name = "Andy";
   print("Hello World, I am $name, Starting my 100 Days of Flutter/Dart");
+  print("My mamma always said, life is like a box of chocolates. You never know what you're gonna get.");
 }
